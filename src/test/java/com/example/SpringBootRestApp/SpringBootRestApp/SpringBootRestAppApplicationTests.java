99630938a8b2a254091bbf7de3522160885fb594
junki.kaneko <junki.kaneko@linecorp.com>
2022-01-19 12:14:56 +0900

    componentの読み込み範囲を変更

--- src/test/java/com/example/SpringBootRestApp/SpringBootRestApp/SpringBootRestAppApplicationTests.java
+++ src/test/java/com/example/SpringBootRestApp/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.SpringBootRestApp;
+package com.example.SpringBootRestApp.SpringBootRestApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
