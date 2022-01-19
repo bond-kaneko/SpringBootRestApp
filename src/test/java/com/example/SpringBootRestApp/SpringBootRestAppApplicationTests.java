189ed95942f7db44659931ac4c7c824f85b58fdb
junki.kaneko <junki.kaneko@linecorp.com>
2022-01-19 12:20:07 +0900

    テスト構造を変更

--- src/test/java/com/example/SpringBootRestApp/SpringBootRestAppApplicationTests.java
+++ src/test/java/com/example/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.SpringBootRestApp.SpringBootRestApp;
+package com.example.SpringBootRestApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
